a9a3d637b9cf21484067b15c0740c647486e8893
Antony Sequeira <asequeira.open@gmail.com>
2024-06-12 16:31:29 -0700

    update versions (#3)

--- scripts/cdktf-install.sh
+++ scripts/cdktf-install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -e
-: ${CDKTF_VERSION:=0.14.3}
-: ${NODE_VERSION:=18.13}
-: ${NVM_VERSION:=v0.39.1}
+: "${CDKTF_VERSION:=0.20.7}"
+: "${NODE_VERSION:=22.2.0}"
+: "${NVM_VERSION:=v0.39.7}"
 
 curl -o- \
   "https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh" \
   | bash
 
-source ~/.nvm/nvm.sh 
 chmod +x ~/.nvm/nvm.sh
+# shellcheck disable=SC1090
+source ~/.nvm/nvm.sh 
 
-nvm install ${NODE_VERSION}
+nvm install "${NODE_VERSION}"
 nvm use node
 
-npm install --global cdktf-cli@${CDKTF_VERSION}
+npm install --global cdktf-cli@"${CDKTF_VERSION}"
 
